b701cda216d7751f27758b4bf7f080d6c686486e
funblaster22 <fogogrl@hotmail.com>
2019-08-13 21:12:55 -0500

    Combine into one function & readibibity improvements

--- play.js
+++ play.js
@@ -1,30 +1,3 @@
-function findBestVersion(file) {
-  var support = {};
-  for (var mod of file.mods) {
-    for (var version of mod.supportedMCversions) {
-      if (support[version] == undefined)
-        support[version] = 1;
-      else
-        support[version] += 1;
-    }
-  }
-  console.log(support);
-  return Object.keys(support).reduce((a, b) => support[a] > support[b] ? a : b);
-}
-
-async function download(name, bestVersion, path) { //TODO: convert to class
-  let data = await newSearch("https://api.cfwidget.com/mc-mods/minecraft/" + name + '?version=' + bestVersion); // '/beta'
-  /*for (var file of data.files) {
-    // TODO: check release/beta/alpha + MC version
-  }*/
-  var file = await newSearch(data.download.url + '/file');
-  fs.writeFile(path + '\\' + name + '.jar', file, "binary", (err) => {
-    if (err)
-      throw err;
-    //incriment loading bar
-  });
-}
-
 function play(target) {
   selectedMod = $(target).parent().prev('input').val();
   var file = editProjectsFile();
@@ -35,9 +8,42 @@ function play(target) {
   console.log(path);
   fs.mkdirSync(path, { recursive: true });
   for (var mod of file.mods) {
-    if (fs.existsSync(path + '\\' + mod.name + '.jar')) continue; // TODO: exception if updateOnRun set
-    download(mod.name, bestVersion, path);
+    var filePath = path + '\\' + mod.name + '.jar';
+    if (fs.existsSync(filePath)) continue; // TODO: exception if updateOnRun set
+    download(mod.name, filePath);
   }
 
-  //start MC
+  //TODO: start MC launcher
+
+
+  function findBestVersion(file) {
+    /* determine MC version with the most supported mods
+    param file: Object from projects.json for selected mod */
+    var support = {}; // stores each MC version and how many mods support it
+    for (var mod of file.mods) {  // iterate through each mod in modpack
+      for (var version of mod.supportedMCversions) {  //
+        if (support[version] == undefined) // if
+          support[version] = 1;
+        else
+          support[version] += 1;
+      }
+    }
+    console.log(support);
+    return Object.keys(support).reduce((a, b) => support[a] > support[b] ? a : b);
+  }
+
+  async function download(modName, filePath) {
+    /* check for compatible mod versions then download */
+    let data = await newSearch("https://api.cfwidget.com/mc-mods/minecraft/" + modName + '?version=' + bestVersion); // '/beta'
+    /*for (var file of data.files) {
+      // TODO: check release/beta/alpha + MC version
+    }*/
+    var file = await newSearch(data.download.url + '/file');
+    fs.writeFile(filePath, file, "binary", (err) => {
+      if (err)
+        throw err;
+      //TODO: incriment loading bar
+    });
+  }
+
 }
